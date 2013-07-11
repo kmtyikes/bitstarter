3aab46262dfe62070b64be9dfc5cccd2f6b46077
Ubuntu <ubuntu@ip-172-31-35-100.us-west-2.compute.internal>
2013-07-11 03:04:17 +0000

    updated web.js

--- web.js
+++ web.js
@@ -3,6 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+  var buf;
+  buf = fs.readFileSync(./index.html);
   response.send('Hello World4!');
 });
 
